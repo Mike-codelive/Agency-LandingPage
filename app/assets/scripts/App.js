286eb29eae0da482aae8808747d780b21d0ea421
Mikeresolve <txrz56ad@live.com.mx>
2021-03-07 10:16:21 -0600

    initial commit

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css'
 import 'lazysizes'
+import 'swiper/swiper-bundle.css';
+import Swiper from 'swiper';
 import MobileMenu from './modules/MobileMenu'
-import RevealOnScroll from './modules/RevealOnScroll'
-import StickyHeader from './modules/StickyHeader'
+// import RevealOnScroll from './modules/RevealOnScroll'
+// import StickyHeader from './modules/StickyHeader'
 import NavLinkReveal from './modules/NavLinkReveal'
 
 // new StickyHeader();
@@ -11,6 +13,29 @@ new NavLinkReveal();
 // new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new MobileMenu();
 let modal
+
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 document.querySelectorAll(".open-modal").forEach(el => {
   el.addEventListener("click", e => {
