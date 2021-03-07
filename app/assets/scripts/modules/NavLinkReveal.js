a7fd3a26a4ba80799af923b285e4b5ba0870ca01
Mikeresolve <txrz56ad@live.com.mx>
2021-03-07 00:05:17 -0600

    navbar responsive support added

--- app/assets/scripts/modules/NavLinkReveal.js
+++ app/assets/scripts/modules/NavLinkReveal.js
@@ -1,19 +1,31 @@
 class NavLinkReveal {
 	constructor() {
-		this.navLink = document.getElementById('dropcontent');
-		this.content = document.querySelector('.navbar__content');
-		this.events()
-		// console.log(this.content);
+		this.navLink = document.querySelector('.navbar__links');
+		this.contents = document.querySelectorAll('.navbar__content');
+		this.dropContent = document.querySelectorAll('.navbar__dropcontent');
+		this.events();
 	}
 
 	events() {
-		this.navLink.addEventListener("click", () => this.toggleContent());
+		this.navLink.addEventListener("click", e => this.toggleContent(e));
 	}
 
-	toggleContent() {
-		this.content.classList.toggle("navbar__content--expanded");
-		this.navLink.classList.toggle("navbar__dropcontent--active");
+	toggleClasses(e) {
+		e.target.nextElementSibling.classList.toggle('navbar__content--expanded');
+		e.target.classList.toggle("navbar__dropcontent--active");
 	}
+
+	toggleContent(e) {
+		if (e.target.classList.contains('navbar__dropcontent--active')) {
+			this.toggleClasses(e);
+		}	else if (e.target.classList.contains('navbar__dropcontent')) {
+			this.contents.forEach(item => item.classList.remove('navbar__content--expanded'));
+			this.dropContent.forEach(item => item.classList.remove('navbar__dropcontent--active'));
+
+			this.toggleClasses(e);
+		}
+	}
+
 }
 
 export default NavLinkReveal;
