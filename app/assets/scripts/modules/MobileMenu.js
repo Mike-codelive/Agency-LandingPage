a6329e1e18bca2b43ae15c65cdd4afba36dd095b
Mikeresolve <txrz56ad@live.com.mx>
2021-03-10 19:24:45 -0600

    arrow animation added

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,20 +1,31 @@
 class MobileMenu {
   constructor() {
-    this.menuIcon = document.querySelector(".navbar__menu-icon")
-    this.menuContent = document.querySelector(".navbar__links")
+    this.menuIcon = document.querySelector(".navbar__menu-icon");
+    this.menuContent = document.querySelector(".navbar__links");
+    this.bodyElement = document.querySelector('body');
     // this.siteHeader = document.querySelector(".site-header")
     this.events()
   }
 
   events() {
     this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+
   }
 
   toggleTheMenu() {
     this.menuContent.classList.toggle("navbar__links--is-visible")
     this.menuIcon.classList.toggle("navbar__menu-icon--close-x")
+    this.bodyElement.classList.toggle('noscroll');
     // this.siteHeader.classList.toggle("site-header--is-expanded")
   }
+
 }
 
 export default MobileMenu
+
+
+// get the current page position window.pageYOffset
+  // window.scrollTo( 0, window.pageYOffset )
+    // window.addEventListener('touchmove', function (event) {
+    //   event.preventDefault()
+    // }, {passive: false})
--- app/assets/scripts/modules/NavLinkReveal.js
+++ app/assets/scripts/modules/NavLinkReveal.js
@@ -3,6 +3,7 @@ class NavLinkReveal {
 		this.navLink = document.querySelector('.navbar__links');
 		this.contents = document.querySelectorAll('.navbar__content');
 		this.dropContent = document.querySelectorAll('.navbar__dropcontent');
+		this.arrows = document.querySelectorAll('.fa-caret-down');
 		this.events();
 	}
 
@@ -13,16 +14,22 @@ class NavLinkReveal {
 	toggleClasses(e) {
 		e.target.nextElementSibling.classList.toggle('navbar__content--expanded');
 		e.target.classList.toggle("navbar__dropcontent--active");
+		e.target.firstElementChild.classList.toggle('fa-caret-down--active');
+		// e.target.nextElementSibling.nextElementSibling.classList.toggle("fa-caret-down--active");
+		console.log(e.target.firstElementChild);
 	}
 
 	toggleContent(e) {
-		if (e.target.classList.contains('navbar__dropcontent--active')) {
-			this.toggleClasses(e);
-		}	else if (e.target.classList.contains('navbar__dropcontent')) {
-			this.contents.forEach(item => item.classList.remove('navbar__content--expanded'));
-			this.dropContent.forEach(item => item.classList.remove('navbar__dropcontent--active'));
-
-			this.toggleClasses(e);
+		/*prevent link dropdown content on large screeens*/
+		if (window.innerWidth < 1010) {
+			if (e.target.classList.contains('navbar__dropcontent--active')) {
+				this.toggleClasses(e);
+			}	else if (e.target.classList.contains('navbar__dropcontent')) {
+				this.contents.forEach(item => item.classList.remove('navbar__content--expanded'));
+				this.dropContent.forEach(item => item.classList.remove('navbar__dropcontent--active'));
+				this.arrows.forEach(item => item.classList.remove('fa-caret-down--active'));
+				this.toggleClasses(e);
+			}
 		}
 	}
 
