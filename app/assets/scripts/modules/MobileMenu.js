73c5c7f132a7b65a858cbca2b787f77778eeed29
Mikeresolve <txrz56ad@live.com.mx>
2021-03-11 14:36:54 -0600

    navbar styles finished & noscroll is support added

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -9,27 +9,12 @@ class MobileMenu {
 
   events() {
     this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
-    // window.scrollTo( 0, window.pageYOffset )
-    // this.bodyElement.addEventListener('touchmove', function (event) {
-    //   event.preventDefault()
-    // }, {passive: false})
   }
 
   toggleTheMenu() {
-    if (!this.menuContent.classList.contains("navbar__links--is-visible")) {
-      const offsetY = window.pageYOffset;
-      document.body.style.top = `${-offsetY}px`;
-      document.body.classList.add('noscroll');
-
-    } else {
-      const offsetY = Math.abs(parseInt(document.body.style.top || 0, 10));
-      document.body.classList.remove('noscroll');
-      document.body.style.removeProperty('top');
-      window.scrollTo(0, offsetY || 0);
-    }
     this.menuContent.classList.toggle("navbar__links--is-visible")
     this.menuIcon.classList.toggle("navbar__menu-icon--close-x")
-    // this.bodyElement.classList.toggle('noscroll');
+    this.bodyElement.classList.toggle('noscroll');
     // this.siteHeader.classList.toggle("site-header--is-expanded")
   }
 
@@ -37,6 +22,20 @@ class MobileMenu {
 
 export default MobileMenu
 
+
+/*check if menu is open and scroll back to current y screen position*/
+
+    // if (!this.menuContent.classList.contains("navbar__links--is-visible")) {
+    //   const offsetY = window.pageYOffset;
+    //   document.body.style.top = `${-offsetY}px`;
+    //   document.body.classList.add('noscroll');
+
+    // } else {
+    //   const offsetY = Math.abs(parseInt(document.body.style.top || 0, 10));
+    //   document.body.classList.remove('noscroll');
+    //   document.body.style.removeProperty('top');
+    //   window.scrollTo(0, offsetY || 0);
+    // }
 
 // get the current page position window.pageYOffset
   // window.scrollTo( 0, window.pageYOffset )
