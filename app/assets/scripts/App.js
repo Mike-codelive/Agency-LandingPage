36ff0885ae644ab049537bea979135521ac96107
Mikeresolve <txrz56ad@live.com.mx>
2021-03-13 15:43:43 -0600

    styles finished & swiper config

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,6 @@
 import '../styles/styles.css'
 import 'lazysizes'
-import Swiper from './modules/Swiper';
+import {heroSwiper, brandSwiper} from './modules/Swiper';
 import MobileMenu from './modules/MobileMenu'
 // import RevealOnScroll from './modules/RevealOnScroll'
 // import StickyHeader from './modules/StickyHeader'
@@ -11,74 +11,25 @@ import AnimationPrevent from './modules/AnimationPrevent'
 // new StickyHeader();
 new NavLinkReveal();
 new AnimationPrevent();
+new MobileMenu();
 // new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 // new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
-new MobileMenu();
-let modal
 
 
-
-let heroSwiper = new Swiper('.swiper-hero', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  grabCursor: true,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  },
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  }
-})
-
-let brandSwiper = new Swiper('.swiper-brands', {
-  // Optional parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
-  direction: 'horizontal',
-  loop: true,
-  grabCursor: true,
-  navigation: {
-    prevEl: '.button-brand-prev',
-    nextEl: '.button-brand-next', 
-  },
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    924: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-  }
-
-})
-
-
-document.querySelectorAll(".open-modal").forEach(el => {
-  el.addEventListener("click", e => {
-    e.preventDefault()
-    if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-        modal = new x.default()
-        setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("There was a problem."))
-    } else {
-      modal.openTheModal()
-    }
-  })
-})
+// let modal
+// document.querySelectorAll(".open-modal").forEach(el => {
+//   el.addEventListener("click", e => {
+//     e.preventDefault()
+//     if (typeof modal == "undefined") {
+//       import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+//         modal = new x.default()
+//         setTimeout(() => modal.openTheModal(), 20)
+//       }).catch(() => console.log("There was a problem."))
+//     } else {
+//       modal.openTheModal()
+//     }
+//   })
+// })
 
 if (module.hot) {
   module.hot.accept()
--- app/assets/scripts/modules/Swiper.js
+++ app/assets/scripts/modules/Swiper.js
@@ -6,12 +6,49 @@ Swiper.use([Navigation, Pagination, Autoplay, EffectFade ]);
 // import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
 
+export let heroSwiper = new Swiper('.swiper-hero', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+  	delay: 3500,
+  	disableOnInteraction: false,
+  },
+  effect: 'fade',
+  fadeEffect: {
+  	crossFade: true
+  }
+})
 
-class BuildSwiper {
-	constructor(swiperName, ObjOptions) {
-		this.swiper = new Swiper(swiperName, ObjOptions);
-	}
-}
+export let brandSwiper = new Swiper('.swiper-brands', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  navigation: {
+  	prevEl: '.button-brand-prev',
+  	nextEl: '.button-brand-next', 
+  },
+  autoplay: {
+  	delay: 1500,
+  	disableOnInteraction: false,
+  },
+  breakpoints: {
+  	640: {
+  		slidesPerView: 2,
+  		spaceBetween: 0,
+  	},
+  	768: {
+  		slidesPerView: 4,
+  		spaceBetween: 0,
+  	},
+  	924: {
+  		slidesPerView: 4,
+  		spaceBetween: 0,
+  	},
+  }
 
-
-export default BuildSwiper
+})
